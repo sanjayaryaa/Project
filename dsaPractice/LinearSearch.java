1aadc289b23683d6f10d08a4c723d824389ab6be
Sanjay <sanjayrabidas04@gmail.com>
2024-09-01 23:41:39 +0530

    user input LINEAR SEARCH

--- dsaPractice/LinearSearch.java
+++ dsaPractice/LinearSearch.java
@@ -1,16 +1,26 @@
 package dsaPractice;
 
+import java.util.Scanner;
+
 public class LinearSearch {
 
     public static void main(String args[]){
-    int arr[] = new int[]{2,5,3,6,7,8,10};
 
-    int targetValue = 6;
+    int arr[] = new int[5];
+    Scanner sc = new Scanner(System.in);
 
+    System.out.println("Enter 5 values in the array: ");
+    for(int j=0; j<=arr.length-1; j++){
+        arr[j] = sc.nextInt();
+    }
+
+    System.out.println("Enter your target value: ");
+    int targetValue = sc.nextInt();
+    
     for(int i=0; i<=arr.length-1; i++){
 
         if(arr[i] == targetValue){
-            System.out.println("Value Found");
+            System.out.println("Target Value i.e. "+targetValue+" Found at Index "+i);
         }
 
     }
