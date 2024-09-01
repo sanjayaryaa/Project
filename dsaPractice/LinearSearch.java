a43e08a44beafd97d9f12b4bfd87c6a9725154ac
Sanjay <sanjayrabidas04@gmail.com>
2024-09-01 23:10:46 +0530

    LINEAR SEARCH PROGRAM

--- dsaPractice/LinearSearch.java
+++ dsaPractice/LinearSearch.java
@@ -1,5 +1,20 @@
 package dsaPractice;
 
 public class LinearSearch {
+
+    public static void main(String args[]){
+    int arr[] = new int[]{2,5,3,6,7,8,10};
+
+    int targetValue = 6;
+
+    for(int i=0; i<=arr.length-1; i++){
+
+        if(arr[i] == targetValue){
+            System.out.println("Value Found");
+        }
+
+    }
+
+    }
     
 }
